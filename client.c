d3a808f51910cd710fe352ca4ae25e046fd78877
Dimitre Dimitrov <didimitr@c1r10s2.42luxembourg.lu>
2025-01-16 20:38:45 +0100

    02

--- client.c
+++ client.c
@@ -6,25 +6,42 @@
 /*   By: didimitr <didimitr@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:18:03 by didimitr          #+#    #+#             */
-/*   Updated: 2025/01/16 00:48:33 by didimitr         ###   ########.fr       */
+/*   Updated: 2025/01/16 18:06:51 by didimitr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int *atob(char *c)
+void bitshift(char c, int pid)
 {
     int i;
-    int a;
 
-    i = 0;
-    a = malloc(8);    
-
+    i = 7;
+    while(i >= 0)
+    {
+        if(((c >> i) & 1) == 0)
+            kill(pid, SIGUSR1);i
+        else
+            kill(pid, SIGUSR2);
+        i--;
+        usleep(1000);
+    }
+}
+void    msgsend(int pid, char *str)
+{
+    
+    while(*str != '\0')
+    {
+        bitshift(*str, pid);
+        str++;
+    }
+    bitshift('\0', pid);
 }
 
-int atoi(char *s)
+int ft_atoi(char *s)
 {
     int i;
 
@@ -41,9 +58,10 @@ int main(int argc,char *argv[])
 {
     int i;
     
-    i = atoi(argv[1]);
-    if(argc != 2)
+    if(argc != 3)
         return(printf("Incorrect Input"));
+    i = ft_atoi(argv[1]);
     printf("%d", i);
+    msgsend(i, argv[2]);
     return(0);
 }
--- server.c
+++ server.c
@@ -6,32 +6,49 @@
 /*   By: didimitr <didimitr@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 13:58:33 by didimitr          #+#    #+#             */
-/*   Updated: 2025/01/15 22:39:25 by didimitr         ###   ########.fr       */
+/*   Updated: 2025/01/16 17:08:59 by didimitr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
+#include <unistd.h>
 
-void handler0(int sig)
+void	assembler(int sig)
 {
-    write(1, "0", 1);
+	static int	i = 0;
+	static char	c = 0;
+
+	if (sig == SIGUSR2)
+		c |= 1 << i++;
+	else
+		i++;
+	if (i == 8)
+	{
+		write(1, &c, 1);
+		i = 0;
+		c = 0;
+	}
 }
-void handler1(int sig)
+
+void	handler0(void)
 {
-    write(1, "1", 1);
+	write(1, "0", 1);
+}
+void	handler1(void)
+{
+	write(1, "1", 1);
 }
 
-int main(void)
+int	main(void)
 {
-    int i;
-    signal(SIGUSR1, handler0);
-    signal(SIGUSR2, handler1);
+	int i;
+	signal(SIGUSR1, &assembler);
+	signal(SIGUSR2, &assembler);
 
-    i = getpid();
-    printf("PID:%d \n", i);    
-    while(1)
-        pause();
-    return(1);       
+	i = getpid();
+	printf("PID:%d \n", i);
+	while (1)
+		pause();
+	return (1);
 }
